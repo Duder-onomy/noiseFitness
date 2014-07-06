1e9aacacfd99f0c0bf477818a9c26f61b12f0d59
Greg Larrenaga <greg.larrenaga@thinksolid.com>
2014-07-06 16:43:50 -0700

    Build: 7 Branch: master  0.1.0 SHA: 3cb2db4

--- public/views/index/view.js
+++ public/views/index/view.js
@@ -2,6 +2,23 @@ define(['masseuse', 'index/options'], function(masseuse, options) {
     'use strict';
 
     return masseuse.plugins.rivets.RivetsView.extend({
-        defaultOptions :  options
+        defaultOptions :  options,
+        afterRender : afterRender,
+        toggleLoading : toggleLoading
     });
+    
+    function afterRender() {
+        window.ivm = this.model;
+    }
+    
+    function toggleLoading() {
+        var currentVal = this.model.get('loading');
+                                        
+        if(currentVal) {
+            this.model.set('loading', false);    
+        } else {
+            this.model.set('loading', true);   
+        }
+                    
+    }
 });
